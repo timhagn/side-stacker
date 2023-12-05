c717d5486f602fcee5933a2d452585b851b64310
Tim Hagn <mail@timhagn.com>
2023-12-05 08:17:05 +0100

    add further comments in socketHelpers

--- src/utils/socketHelpers.ts
+++ src/utils/socketHelpers.ts
@@ -2,23 +2,64 @@ import crypto from 'crypto'
 import { GameStack } from '@/types/dbTypes'
 import { GamePieceStates, PlayStates } from '@/types/gameStateTypes'
 
-export const randomId = () => crypto.randomBytes(8).toString('hex')
+/**
+ * Creates a new session ID.
+ *
+ * @returns string
+ */
+export const randomId = (): string => crypto.randomBytes(8).toString('hex')
 
-export const getOpposingPlayer = (player: string, gameState: GameStack) =>
-  player === gameState.playerOne ? gameState.playerTwo : gameState.playerOne
+/**
+ * Determines & returns the opposing player's ID.
+ *
+ * @param {string}      currentPlayer   The current player.
+ * @param {GameStack}   gameState       The current GameStack state.
+ * @returns string
+ */
+export const getOpposingPlayer = (
+  currentPlayer: string,
+  gameState: GameStack,
+): string =>
+  currentPlayer === gameState.playerOne
+    ? gameState.playerTwo
+    : gameState.playerOne
 
-export const isPlayerTwo = (player: string, gameState: GameStack) =>
+/**
+ * Determines if a given player is the second player.
+ *
+ * @param {string}      player      The player to check.
+ * @param {GameStack}   gameState   The current GameStack state.
+ * @returns boolean
+ */
+export const isPlayerTwo = (player: string, gameState: GameStack): boolean =>
   player === gameState?.playerTwo
 
+/**
+ * Returns the Game Piece state for a given player.
+ *
+ * @param {string}      player      The player to check against.
+ * @param {GameStack}   gameState   The current GameStack state.
+ * @returns GamePieceStates
+ */
 export const getGamePieceStateForPlayer = (
   player: string,
   gameState: GameStack,
-) =>
+): GamePieceStates =>
   player === gameState?.playerOne
     ? GamePieceStates.playerOne
     : GamePieceStates.playerTwo
 
-export const getWinningPlayerState = (player: string, gameState: GameStack) =>
+/**
+ * Returns the winning play state for a given player.
+ *
+ * @param {string}      player      The player to check against.
+ * @param {GameStack}   gameState   The current GameStack state.
+ * @returns PlayStates
+ */
+export const getWinningPlayerState = (
+  player: string,
+  gameState: GameStack,
+): PlayStates =>
   player === gameState?.playerOne
     ? PlayStates.playerOneWon
     : PlayStates.playerTwoWon
