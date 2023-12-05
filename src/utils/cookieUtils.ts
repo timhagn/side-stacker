cd4e8b5687d1e348c10ce1d367e3b9c05fe98187
Tim Hagn <mail@timhagn.com>
2023-12-05 08:19:25 +0100

    add further comments in cookieHelpers

--- src/utils/cookieUtils.ts
+++ src/utils/cookieUtils.ts
@@ -1,12 +1,22 @@
 export const SESSION_ID_COOKIE_NAME = 'sessionId'
 
+/**
+ * Sets the Cookie of SESSION_ID_COOKIE_NAME to a given session ID.
+ *
+ * @param {string}  sessionId   The session ID (player ID) to save in the cookie.
+ */
 export const setSessionIdCookie = (sessionId: string) => {
   if (typeof window !== 'undefined') {
     document.cookie = `${SESSION_ID_COOKIE_NAME}=${sessionId}; SameSite=None; Secure`
   }
 }
 
-export const getSessionIdCookie = () => {
+/**
+ * Returns the value of the Cookie of SESSION_ID_COOKIE_NAME.
+ *
+ * @returns string | undefined
+ */
+export const getSessionIdCookie = (): string | undefined => {
   if (typeof window !== 'undefined') {
     return document.cookie
       .split('; ')
