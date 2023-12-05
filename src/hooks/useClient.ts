0cbeec4dcb6f065ae8537afb3c72555ecb128d4b
Tim Hagn <mail@timhagn.com>
2023-12-05 08:21:38 +0100

    add further comments in useClient

--- src/hooks/useClient.ts
+++ src/hooks/useClient.ts
@@ -1,6 +1,12 @@
 import { useEffect, useMemo, useState } from 'react'
 
-export const useClient = () => {
+/**
+ * Hook to determine if we are on the Client (as useEffect() isn't called on the
+ * Server) to prevent Render Errors.
+ *
+ * @returns boolean
+ */
+export const useClient = (): boolean => {
   const [isClient, setIsClient] = useState(false)
   useEffect(() => {
     setIsClient(true)
